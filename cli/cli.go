ad78ef1eebaba848e5268de18f9073aa6a24ebe3
Maciej Kapusta <maciej.kapusta@codewise.com>
2017-11-13 09:27:00 +0100

    gitignore and some tweaks

--- cli/cli.go
+++ cli/cli.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	user2 "os/user"
 )
 
 type CliFlags struct {
@@ -15,16 +16,19 @@ type CliFlags struct {
 }
 
 func ParseFlags() *CliFlags {
-
+	current, e := user2.Current()
+	if e != nil {
+		panic(e)
+	}
 	profile := flag.String("profile", "", "Optional AWS Profile. If missing the profile from your AWS CLI used.")
-	user := flag.String("user", "", "ssh user (required)")
+	user := flag.String("user", current.Username, "ssh user. If empty your login will be used")
 	instanceNamePart := flag.String("instance", "", "part of instance name to be matched(required)")
 	outputFile := flag.String("out", "", "Optional output file to save instances and use in ssh. If missing std out will be used")
-	region := flag.String("region", "", "Optional AWS region. If absent the AWS CLI config is used.")
+	region := flag.String("region", "us-east-1", "Optional AWS region. If absent the AWS CLI config is used.")
 	flag.Parse()
 
-	if *user == "" || *instanceNamePart == "" {
-		fmt.Fprintln(os.Stderr, "Missing parameters. Usage: boxlister -user=joe.doe [-profile=acme-prod] -instance=db [-out=my_ssh_file]")
+	if *instanceNamePart == "" {
+		fmt.Fprintln(os.Stderr, "Missing parameters. Usage: boxlister [-user=joe.doe] [-profile=acme-prod] -instance=db [-out=my_ssh_file]")
 		flag.PrintDefaults()
 		return nil
 	}
